31bf0703f4ac7950791cdd7aa4b904eb81837dba
meylani <meylanisaputri94@gmail.com>
2026-09-14 07:49:15 +0700

    Add Vercel serverless entry point and configuration

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const swaggerUi = require("swagger-ui-express");
 const swaggerSpec = require("./config/swagger");
 const todoRoutes = require("./routes/todo.routes");
@@ -7,16 +8,18 @@ const statsRoutes = require("./routes/stats.routes");
 const logger = require("./middlewares/logger.middleware");
 const notFound = require("./middlewares/notFound.middleware");
 const errorHandler = require("./middlewares/errorHandler.middleware");
- 
+
 const app = express();
- 
+
+app.use(cors());
 app.use(logger);
 app.use(express.json());
- 
+
 app.get("/", (req, res) => {
   res.json({ message: "Todo API is running" });
 });
- 
+
+// Halaman dokumentasi interaktif tersedia di /api-docs
 const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.0.0/swagger-ui.min.css";
 const JS_URL = [
   "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.0.0/swagger-ui-bundle.js",
@@ -26,14 +29,17 @@ const JS_URL = [
 app.use(
   "/api-docs",
   swaggerUi.serve,
-  swaggerUi.setup(swaggerSpec, { customCssUrl: CSS_URL, customJs: JS_URL })
+  swaggerUi.setup(swaggerSpec, {
+    customCssUrl: CSS_URL,
+    customJs: JS_URL,
+  })
 );
- 
+
 app.use("/api/auth", authRoutes);
 app.use("/api/todos", todoRoutes);
 app.use("/api/stats", statsRoutes);
- 
+
 app.use(notFound);
 app.use(errorHandler);
- 
+
 module.exports = app;

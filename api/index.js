3f4836d4496a391d630af7f74ae3d8b600f1f476
meylani <meylanisaputri94@gmail.com>
2026-09-17 10:33:10 +0700

    fix vercel api docs

--- api/index.js
+++ api/index.js
@@ -1,26 +1,5 @@
 require("dotenv").config();
 
 const app = require("../src/app");
-const connectDB = require("../src/config/db");
 
-let dbConnection;
-
-async function handler(req, res) {
-  try {
-    if (!dbConnection) {
-      dbConnection = connectDB();
-    }
-
-    await dbConnection;
-
-    return app(req, res);
-  } catch (error) {
-    console.error("Database connection error:", error);
-    return res.status(500).json({
-      success: false,
-      message: "Database connection failed",
-    });
-  }
-}
-
-module.exports = handler;
+module.exports = app;

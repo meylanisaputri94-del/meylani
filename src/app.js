230222704bf7736964c53300e98d7297c11aebda
meylani <meylanisaputri94@gmail.com>
2026-09-17 10:37:45 +0700

    fix swagger ui

--- src/app.js
+++ src/app.js
@@ -2,9 +2,11 @@ const express = require("express");
 const cors = require("cors");
 const swaggerUi = require("swagger-ui-express");
 const swaggerSpec = require("./config/swagger");
+
 const todoRoutes = require("./routes/todo.routes");
 const authRoutes = require("./routes/auth.routes");
 const statsRoutes = require("./routes/stats.routes");
+
 const logger = require("./middlewares/logger.middleware");
 const notFound = require("./middlewares/notFound.middleware");
 const errorHandler = require("./middlewares/errorHandler.middleware");
@@ -19,13 +21,14 @@ app.get("/", (req, res) => {
   res.json({ message: "Todo API is running" });
 });
 
-// Halaman dokumentasi interaktif tersedia di /api-docs
+// Swagger UI
 app.use(
   "/api-docs",
   swaggerUi.serve,
   swaggerUi.setup(swaggerSpec)
 );
 
+// API Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/todos", todoRoutes);
 app.use("/api/stats", statsRoutes);

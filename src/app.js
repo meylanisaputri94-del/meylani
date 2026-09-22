02ad4f84d9b9d463e310273b00d41a8a880a6a31
meylani <meylanisaputri94@gmail.com>
2026-09-22 11:59:21 +0700

    fix vercel function export

--- src/app.js
+++ src/app.js
@@ -1,51 +1,26 @@
-const express = require("express");
-const cors = require("cors");
+require("dotenv").config();
 
-const swaggerUi = require("swagger-ui-express");
-const swaggerSpec = require("./config/swagger");
+const app = require("../src/app");
+const connectDB = require("../src/config/db");
 
-const todoRoutes = require("./routes/todo.routes");
-const authRoutes = require("./routes/auth.routes");
-const statsRoutes = require("./routes/stats.routes");
+let isConnected = false;
 
-const app = express();
+module.exports = (req, res) => {
+  if (!isConnected) {
+    return connectDB()
+      .then(() => {
+        isConnected = true;
+        app(req, res);
+      })
+      .catch((error) => {
+        console.error("Serverless error:", error);
 
-app.use(cors());
-app.use(express.json());
+        res.status(500).json({
+          message: "Internal Server Error",
+          error: error.message
+        });
+      });
+  }
 
-// Home
-app.get("/", (req, res) => {
-  res.json({
-    message: "Todo API is running"
-  });
-});
-
-// Swagger UI biasa
-app.use(
-  "/api-docs",
-  swaggerUi.serve,
-  swaggerUi.setup(swaggerSpec)
-);
-
-// API Routes
-app.use("/api/auth", authRoutes);
-app.use("/api/todos", todoRoutes);
-app.use("/api/stats", statsRoutes);
-
-// 404
-app.use((req, res) => {
-  res.status(404).json({
-    message: "Route not found"
-  });
-});
-
-// Error handler
-app.use((err, req, res, next) => {
-  console.error(err);
-
-  res.status(err.status || 500).json({
-    message: err.message || "Internal Server Error"
-  });
-});
-
-module.exports = app;
+  return app(req, res);
+};
